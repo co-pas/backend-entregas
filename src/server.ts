e5d33a3056bf4f2b9f1edb35fa7a6d4b90fb1763
Natan Fernandes <natangt11@gmail.com>
2022-11-23 11:47:17 -0300

    Melhorando a trativa de erros

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import routes from "./routes";
+import "express-async-errors";
+
 import Settings from "./config/settings";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+      return response.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "internal server error.",
+    });
+  }
+);
 
 app.listen(Settings.port, Settings.hostname, () => {
   console.log(Settings.startMessage);
